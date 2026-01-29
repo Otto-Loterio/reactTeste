a57d8ea703716fc2ed5cfbdfb360e8bf17e8265c
Otto Lotério <ottopequenostal@gmail.com>
2026-01-29 16:59:05 +0000

    Primeiro Commit

---
+++ aplicacaoReact/teste-instalacao/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá mundo</h1>
+      <p>Meu primeiro site utilizando-se React!</p>
+    </div>
+  );
+}
+
+export default App;
